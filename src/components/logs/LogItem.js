a322bce8d4515d73cb7a91fa963a0e7e98a1c87e
lakshyavijh <lakshya.vijh99@gmail.com>
2020-03-31 19:19:31 +0530

    delete logs

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Moment from 'react-moment';
+import { connect } from 'react-redux';
 import M from 'materialize-css/dist/js/materialize.min.js';
+import { deleteLog, setCurrent } from '../../actions/logActions';
 
-const LogItem = ({log}) => {
+
+const LogItem = ({ log, deleteLog, setCurrent }) => {
     const onDelete = () => {
-        // deleteLog(log.id);
+        deleteLog(log.id);
         M.toast({ html: 'Log Deleted' });
       };
     return (
@@ -32,14 +35,14 @@ const LogItem = ({log}) => {
           </div>
         </li>
       );
-    
-    
-    LogItem.propTypes = {
-      log: PropTypes.object.isRequired,
-     
-    };
-    
-
-}
-
-export default LogItem
+   };
+      LogItem.propTypes = {
+        log: PropTypes.object.isRequired,
+        deleteLog: PropTypes.func.isRequired,
+        setCurrent: PropTypes.func.isRequired
+      };
+      
+      export default connect(
+        null,
+        { deleteLog, setCurrent }
+      )(LogItem);
